268564ab197924b25b0b7d99efa4c7f6b745f133
heatherjc07 <heather_j_milligan@hotmail.com>
2016-11-27 00:13:17 +0000

    Starting to update to add routing

--- app.js
+++ app.js
@@ -1,54 +1,16 @@
-var app = angular.module('githubViewer', []);
-var MainController = function(
-  $scope, github, $interval, $log,
-  $anchorScroll, $location) {
+(function(){
+    
+    var app = angular.module("githubViewer", ["ngRoute"]);
+    
+    app.config(function($routeProvider){
+        $routeProvider
+            .when("/main", {
+                templateUrl: "main.html",
+                controller: "MainController"
+            })
+            .otherwise({redirectTo:"/main"});
+    });
+    
+}());
 
-  var onUserComplete = function(response) {
-    $scope.error = "";
-    $scope.person = response.data;
-    github.getRepos($scope.person.repos_url)
-      .then(onReposComplete, onError);
-  };
 
-  var onReposComplete = function(response) {
-    $scope.repos = response.data;
-    $location.hash("userDetails");
-    $anchorScroll();
-  };
-
-  var onError = function(reason) {
-    $scope.error = "There's been a problem";
-  };
-
-  var decrementCountdown = function() {
-    $scope.countdown -= 1;
-    if ($scope.countdown < 1) {
-      $scope.search($scope.username);
-    }
-  };
-
-  var countDownInterval = null;
-  var startCountdown = function() {
-    countDownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
-  };
-
-  $scope.search = function(username) {
-    $log.info("searching for: " + username);
-    github.getUser(username)
-      .then(onUserComplete, onError);
-    if (countDownInterval) {
-      $interval.cancel(countDownInterval);
-      $scope.countdown = null;
-    }
-  };
-
-  startCountdown();
-};
-
-$scope.message = "Github G Viewer";
-$scope.repoSortOrder = "-stargazers_count";
-$scope.username = "angular";
-$scope.countdown = 5;
-app.controller("MainController", MainController);
-
-app.controller("MainController", MainController);
